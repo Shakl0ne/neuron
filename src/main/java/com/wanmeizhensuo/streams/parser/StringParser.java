05788ead0775bb8427616a1c7d9ed7d7af772cdd
Mars Liu <mars.liu@outlook.com>
2021-05-20 14:53:02 +0800

    string token parser

--- src/main/java/com/wanmeizhensuo/streams/parser/StringParser.java
+++ src/main/java/com/wanmeizhensuo/streams/parser/StringParser.java
@@ -1,17 +1,19 @@
 package com.wanmeizhensuo.streams.parser;
 
 import com.google.errorprone.annotations.Var;
+import jaskell.parsec.common.One;
 import jaskell.parsec.common.Parsec;
 
 import jaskell.parsec.common.State;
 import jaskell.parsec.common.TxtState;
 
+import static jaskell.parsec.Atom.one;
 import static jaskell.parsec.common.Combinator.*;
 import static jaskell.parsec.common.Txt.*;
 
-public class StringParser implements Parsec<Character, String> {
-    private final Parsec<Character, Character> character = attempt(chNone("\"\\"));
-    private final Parsec<Character, Character> escapeCharacter = ch('\\').then(s -> {
+public class StringParser implements Parsec<Token, Token> {
+    final Parsec<Character, Character> character = one();
+    final Parsec<Character, Character> escapeCharacter = ch('\\').then(s -> {
         Character c = s.next();
         switch (c) {
             case 'n':
@@ -26,23 +28,19 @@ public class StringParser implements Parsec<Character, String> {
                 throw s.trap(String.format("invalid char  \\%c", c));
         }
     });
-    private String content;
-
-    {
-        try {
-            TxtState s = new TxtState(content, "\n");
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
-        }
-    }
-
-    private Object State;
-    private final Parsec<Character, String> stringParser = between(ch('\''), ch('\''),
-            many(choice(character, escapeCharacter))).bind(joining());
-
+    final Parsec<Character, String> parser = between(ch('\''), ch('\''),
+            many(choice(attempt(escapeCharacter), character))).bind(joinChars());
 
     @Override
-    public String parse(String content) throws Throwable {
-        return Parsec.super.parse(content);
+    public Token parse(State<Token> s) throws Throwable {
+        var token = s.next();
+        if (token.type != TokenType.STRING){
+            var message = String.format("expect a string token but get %s type %s",
+                    token.content, token.type);
+            throw s.trap(message);
+        }
+        var state = new TxtState(token.content.toString());
+        var content = parser.parse(state);
+        return new Token(content, TokenType.STRING);
     }
 }
